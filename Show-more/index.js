fa79da247a2c0c092949f4482e52d21472e43add
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-14 12:19:35 +0545

    event & window

--- Show-more/index.js
+++ Show-more/index.js
@@ -1,25 +1,31 @@
-const content = document.querySelector(".container");
+function setUpEvents() {
+  const content = document.querySelector(".container");
 
-const clickButton = document.querySelector("button");
+  const clickButton = document.querySelector("button");
 
-clickButton.addEventListener("click", showMagic);
+  clickButton.addEventListener("click", showMagic);
 
-function showMagic() {
-  if (content.classList.contains("distance")) {
-    content.classList.remove("distance");
-    clickButton.innerText = "Show More";
-  } else {
-    content.className += " distance";
-    clickButton.innerText = "Show Less";
+  function showMagic() {
+    if (content.classList.contains("distance")) {
+      content.classList.remove("distance");
+      clickButton.innerText = "Show More";
+    } else {
+      content.className += " distance";
+      clickButton.innerText = "Show Less";
+    }
   }
+
+  // function showMagic() {
+  //   if (content.getAttribute("id") == "distance") {
+  //     content.removeAttribute("id");
+  //     clickButton.innerText = "Show More";
+  //   } else {
+  //     content.setAttribute("id", "distance");
+  //     clickButton.innerText = "Show Less";
+  //   }
+  // }
 }
 
-// function showMagic() {
-//   if (content.getAttribute("id") == "distance") {
-//     content.removeAttribute("id");
-//     clickButton.innerText = "Show More";
-//   } else {
-//     content.setAttribute("id", "distance");
-//     clickButton.innerText = "Show Less";
-//   }
-// }
+window.onload = function () {
+  setUpEvents();
+};
